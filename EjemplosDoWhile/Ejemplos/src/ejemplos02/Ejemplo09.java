fd47d61d5a7bec922cc8473d894dcd47658ad338
unknown <apadilla2@utpl.edu.ec>
2023-11-23 17:12:43 -0500

    intento fallido

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo09.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo09.java
@@ -22,7 +22,7 @@ public class Ejemplo09 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        
+
         String cadenaFinal;
         int nota;
         int salida;
@@ -31,37 +31,51 @@ public class Ejemplo09 {
         boolean bandera = true;
         int suma = 0;
         int contador_calificaciones = 0;
-        
-        cadenaFinal = "Listado de Notas\n";
-        
-        do {
-            
-            
-            // agrego valor al acumulador
-            suma = suma + nota;
-            // agrego una unidad al contador para luego sacar el promedio
-            contador_calificaciones = contador_calificaciones + 1;
-            
-            
-            System.out.println("Ingrese (-111) si desea salir del ciclo; "
-                    + "cualquier otro número para continuar");
-            salida = entrada.nextInt();
+        int x = 1;
 
-            if (salida == -111) {
-                bandera = false;
+        cadenaFinal = "Listado de Notas\n";
+        System.out.println("Ingrese numero de estudiantes");
+        int numero = entrada.nextInt();
+
+        do {
+
+            System.out.println("Ingrese nombre");
+            nombre = entrada.nextLine();
+            entrada.nextLine();
+            System.out.println("Igrese nota");
+            nota = entrada.nextInt();
+            suma = suma + nota;
+
+            contador_calificaciones = contador_calificaciones + 1;
+
+            if (x == numero) {
+                System.out.println("Ingrese (-111) si desea salir del ciclo; "
+                        + "cualquier otro número para continuar");
+                salida = entrada.nextInt();
+
+                if (salida == -111) {
+                    bandera = false;
+                }
             }
+
+            x = x + 1;
             // atención
             entrada.nextLine(); // se limpia el buffer, pues el primer valor
-                               // que se solicita al inicio del ciclo es una
-                               // cadena
+            // que se solicita al inicio del ciclo es una
+            // cadena
+            promedio = (double) suma / contador_calificaciones;
+            cadenaFinal = String.format("%s\nCalificacion %d "
+                + "del estudiante %s\nPromedio final: %.2f",
+                cadenaFinal,
+                nota,
+                nombre,
+                promedio);
 
         } while (bandera); // (bandera==true)
-        
+
         // promedio = suma / contador_calificaciones;
-        promedio = (double)suma / contador_calificaciones;
-        cadenaFinal = String.format("%s\nPromedio de calificaciones: %.2f",
-                cadenaFinal,
-                promedio);
-        System.out.printf("%s\n", cadenaFinal);
+        
+
+        System.out.printf("\n%s\n", cadenaFinal);
     }
 }
