565bdaa57a0cd3e92cbaf19e387ec2f7e50beb3f
unknown <apadilla2@utpl.edu.ec>
2023-11-23 16:07:25 -0500

    .

--- EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo05.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ El codigo de por si define 3 varibales como enteros:numero,potencia,resultado
+ este permite que se presente el resultado de una potencia previamentes 
+ ingresadas o ya definidas, en este caso el numero es la base=4 y la potencia 
+ es el exponente=3 dando como resultado 64, usando un contador desde 1 hasta que
+tenga el mismo valor que la potencia, primero el resultado es 1 para multiplicar-
+lo con numero dando el mismo resultado que una potencia, cuando el contador pasa
+de la potencia el resultado se escribe solo
  */
 package ejemplos01;
 
@@ -19,9 +23,9 @@ public class Ejemplo05 {
         // 6. resultado, i[0, n]
         int resultado;
         // numero <-- 4
-        numero = 4;
+        numero = 3;
         // 8. potencia <-- 3
-        potencia = 3;
+        potencia = 2;
         // 9. resultado <-- 1
         resultado = 1;
 
